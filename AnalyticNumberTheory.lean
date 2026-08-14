588d013e9b54b700d15f036bd4647a43f78ffc0a
Slava Chan <49863417+UyNewNas@users.noreply.github.com>
2026-08-15 06:48:53 +0800

    fix: move W1LemmaB to Sieve module path

--- AnalyticNumberTheory.lean
+++ AnalyticNumberTheory.lean
@@ -36,7 +36,7 @@ import AnalyticNumberTheory.LargeSieve.PanTypeIAssembly
 import AnalyticNumberTheory.LargeSieve.CharacterIndicators
 import AnalyticNumberTheory.LargeSieve.WellSpaced
 import AnalyticNumberTheory.LargeSieve.Duality
-import W1LemmaB
+import AnalyticNumberTheory.Sieve.W1LemmaB
 
 /-!
 # Analytic Number Theory
