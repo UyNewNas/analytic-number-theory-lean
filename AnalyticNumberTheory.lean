ed75ce686c2c8c1a2a93cb4b9926ae1739738359
Slava Chan <49863417+UyNewNas@users.noreply.github.com>
2026-09-17 05:09:31 +0800

    build: export dyadic von Mangoldt window bridge

--- AnalyticNumberTheory.lean
+++ AnalyticNumberTheory.lean
@@ -20,6 +20,7 @@ import AnalyticNumberTheory.Dirichlet.Moments
 import AnalyticNumberTheory.Dirichlet.MomentBounds
 import AnalyticNumberTheory.Dirichlet.TwistedMoments
 import AnalyticNumberTheory.Dirichlet.PrimeWindowWeights
+import AnalyticNumberTheory.Dirichlet.VonMangoldtWindow
 import AnalyticNumberTheory.Sieve.GoldbachDensity
 import AnalyticNumberTheory.Sieve.SelbergIdentities
 import AnalyticNumberTheory.Sieve.Distribution
