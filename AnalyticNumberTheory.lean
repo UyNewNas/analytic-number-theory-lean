50ca13cc41f73184f07a17b197753d932dcd8400
Slava Chan <49863417+UyNewNas@users.noreply.github.com>
2026-09-17 14:18:36 +0800

    dirichlet: root-export right-half-plane bound

--- AnalyticNumberTheory.lean
+++ AnalyticNumberTheory.lean
@@ -18,6 +18,7 @@ import AnalyticNumberTheory.Dirichlet.CharacterPrefix
 import AnalyticNumberTheory.Dirichlet.AbelWeightVariation
 import AnalyticNumberTheory.Dirichlet.ConditionalValueSeries
 import AnalyticNumberTheory.Dirichlet.LFunctionGrowth
+import AnalyticNumberTheory.Dirichlet.RightHalfPlaneBounds
 import AnalyticNumberTheory.Dirichlet.VonMangoldtLSeries
 import AnalyticNumberTheory.Dirichlet.Orthogonality
 import AnalyticNumberTheory.Dirichlet.Decomposition
