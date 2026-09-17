b9c6ce40e3bbcdfbb00ccdefc1335a3ebc7380f7
Slava Chan <49863417+UyNewNas@users.noreply.github.com>
2026-09-17 22:37:27 +0800

    test(audit): cover Jensen-centre Dirichlet L bounds

---
+++ AuditLFunctionTwoBounds.lean
@@ -0,0 +1,9 @@
+import AnalyticNumberTheory.Dirichlet.LFunctionTwoBounds
+
+/-!
+# Dirichlet Jensen-centre trust audit
+-/
+
+#print axioms AnalyticNumberTheory.Dirichlet.norm_LFunction_sub_one_le_of_two_le_re
+#print axioms AnalyticNumberTheory.Dirichlet.one_third_le_norm_LFunction_of_two_le_re
+#print axioms AnalyticNumberTheory.Dirichlet.LFunction_ne_zero_of_two_le_re
