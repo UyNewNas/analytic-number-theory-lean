a976708899a84d0d44be4f0656ec775afbfb20f4
Slava Chan <49863417+UyNewNas@users.noreply.github.com>
2026-09-17 14:25:24 +0800

    audit: cover L-function anchor bounds

--- AuditDirichlet.lean
+++ AuditDirichlet.lean
@@ -5,6 +5,7 @@ import AnalyticNumberTheory.Dirichlet.AbelWeightVariation
 import AnalyticNumberTheory.Dirichlet.ConditionalValueSeries
 import AnalyticNumberTheory.Dirichlet.LFunctionGrowth
 import AnalyticNumberTheory.Dirichlet.RightHalfPlaneBounds
+import AnalyticNumberTheory.Dirichlet.LFunctionAnchor
 import AnalyticNumberTheory.Dirichlet.VonMangoldtLSeries
 import AnalyticNumberTheory.Dirichlet.Orthogonality
 import AnalyticNumberTheory.Dirichlet.Decomposition
@@ -71,6 +72,9 @@ repository-wide audit.
 #print axioms AnalyticNumberTheory.Dirichlet.orderedValueSeries_eq_LFunction_of_re_pos
 #print axioms AnalyticNumberTheory.Dirichlet.norm_LFunction_le_growth
 #print axioms AnalyticNumberTheory.Dirichlet.norm_dirichletLFunction_le
+#print axioms AnalyticNumberTheory.Dirichlet.delta_quarter_le_norm_LFunction_anchor
+#print axioms AnalyticNumberTheory.Dirichlet.anchor_disk_re_lower
+#print axioms AnalyticNumberTheory.Dirichlet.norm_LFunction_le_on_anchor_disk
 #print axioms AnalyticNumberTheory.Dirichlet.twistedVonMangoldtLSeries_eq_negLogDerivLFunction
 #print axioms AnalyticNumberTheory.Dirichlet.GRHAt.analyticAt_negLogDerivLFunction
 #print axioms AnalyticNumberTheory.Dirichlet.GRHAt.analyticOnNhd_negLogDerivLFunction_halfPlane
