b2c45f509bdb48343b59b6de3fed5ddb8ea92eaa
Slava Chan <49863417+UyNewNas@users.noreply.github.com>
2026-09-17 17:13:10 +0800

    dirichlet: export quantitative Perron right tails

--- AnalyticNumberTheory.lean
+++ AnalyticNumberTheory.lean
@@ -24,6 +24,7 @@ import AnalyticNumberTheory.Dirichlet.FiniteRectangleLogDerivative
 import AnalyticNumberTheory.Dirichlet.VonMangoldtLSeries
 import AnalyticNumberTheory.Dirichlet.TwistedSmoothedPerron
 import AnalyticNumberTheory.Dirichlet.TwistedSmoothedPerronRight
+import AnalyticNumberTheory.Dirichlet.TwistedSmoothedPerronRightTail
 import AnalyticNumberTheory.Dirichlet.Orthogonality
 import AnalyticNumberTheory.Dirichlet.Decomposition
 import AnalyticNumberTheory.Dirichlet.Moments
