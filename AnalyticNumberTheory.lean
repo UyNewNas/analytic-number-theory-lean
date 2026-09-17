4c56dd7a5394a4322c2680bcf9e1bca9ff23a30d
Slava Chan <49863417+UyNewNas@users.noreply.github.com>
2026-09-17 21:09:46 +0800

    chore: export twisted smoothed psi unsmoothing

--- AnalyticNumberTheory.lean
+++ AnalyticNumberTheory.lean
@@ -36,6 +36,7 @@ import AnalyticNumberTheory.Dirichlet.TwistedMoments
 import AnalyticNumberTheory.Dirichlet.PrimeWindowWeights
 import AnalyticNumberTheory.Dirichlet.VonMangoldtWindow
 import AnalyticNumberTheory.Dirichlet.VonMangoldtPartialSums
+import AnalyticNumberTheory.Dirichlet.TwistedSmoothedPsiClose
 import AnalyticNumberTheory.Sieve.GoldbachDensity
 import AnalyticNumberTheory.Sieve.SelbergIdentities
 import AnalyticNumberTheory.Sieve.Distribution
