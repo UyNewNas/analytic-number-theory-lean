74691f8a28d61e58be9975135368e7d1c3a5b695
Slava Chan <49863417+UyNewNas@users.noreply.github.com>
2026-09-17 14:25:03 +0800

    dirichlet: root-export L-function anchor bounds

--- AnalyticNumberTheory.lean
+++ AnalyticNumberTheory.lean
@@ -19,6 +19,7 @@ import AnalyticNumberTheory.Dirichlet.AbelWeightVariation
 import AnalyticNumberTheory.Dirichlet.ConditionalValueSeries
 import AnalyticNumberTheory.Dirichlet.LFunctionGrowth
 import AnalyticNumberTheory.Dirichlet.RightHalfPlaneBounds
+import AnalyticNumberTheory.Dirichlet.LFunctionAnchor
 import AnalyticNumberTheory.Dirichlet.VonMangoldtLSeries
 import AnalyticNumberTheory.Dirichlet.Orthogonality
 import AnalyticNumberTheory.Dirichlet.Decomposition
